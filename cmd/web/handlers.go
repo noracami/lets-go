25fc00d77d069bc2c2046a00f404a79c586527a6
noracami <kerke.2011@gmail.com>
2024-01-28 03:55:39 +0800

    🚚 arrange structure

---
+++ cmd/web/handlers.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"net/http"
+)
+
+// Define a home handler function which writes a byte slice containing
+// "Hello from Snippetbox" as the response body.
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Snippetbox"))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/noracami/lets-go
+
+go 1.21.6
